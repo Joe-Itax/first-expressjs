a943ce196d0969f21801e6ed3c8a445801383c9e
Joseph Itakala <josephitakala18@gmail.com>
2024-03-08 14:01:32 +0100

    add: Listing limité des articles and Suppression de plusieurs articles avec une seule requete

--- index.js
+++ index.js
@@ -66,43 +66,71 @@ app.delete("/articles/:id", (req, res) => {
   }
 });
 
-
 //Modification partielle d'un article
-app.patch("/articles/:id", (req, res) =>{
-  const newTitleOfAnArticle = req.body.title;
-  const {id} = req.params;
-  const articleIndex = findArticleIndex(id);
+app.patch("/articles/:id", (req, res) => {
+  /*const newTitleOfAnArticle = req.body.title;
+  const newArticleKeys = Object.keys(req.body);
+  console.log(newArticleKeys);
+  const { id } = req.params;
+  const articleIndex = findArticleIndex(id);*/
 
-  if(articleIndex < 0){
-    res.status(404).send(`Erreur car l'article avec l'id ${id} n'existe pas.`)
-  }else{
-
+  const { id } = req.params;
+  const body = req.body;
+  const article = findArticleById(id);
+  /*if (articleIndex < 0) {
+    res.status(404).send(`Erreur car l'article avec l'id ${id} n'existe pas.`);
+  } else {
     data[articleIndex].title = newTitleOfAnArticle;
     res.send(data);
+  }*/
+  if (!article)
+    res.status(404).send(`Erreur car l'article avec l'id ${id} n'existe pas.`);
+
+  for (const key in body) {
+    if (!article[key]) {
+      res.status(400).send("Erreur de données");
+    }
+    article[key] = body[key];
   }
-})
 
-
+  res.send(article);
+});
 
 //Listing limité des articles
-app.get("/articles", (req, res) =>{
+app.get("/articles", (req, res) => {
   const page = req.query.page || 1;
-  const pageSize = req.query.pageSize || 5;
+  const articlePerPage = req.query.articlePerPage || 5;
 
-  const startIndex = (page - 1) * pageSize;
-  const endIndex = page * pageSize;
+  console.log(req);
+
+  const startIndex = (page - 1) * articlePerPage;
+  const endIndex = page * articlePerPage;
 
   const pageArticles = data.slice(startIndex, endIndex);
-  res.send(pageArticles);
-})
 
+  res.json({
+    totalPages: Math.ceil(data.length / articlePerPage),
+    currentPage: page,
+    articlePerPage: articlePerPage,
+    totalArticles: data.length,
+    articles: pageArticles,
+  });
+  // res.send(pageArticles);
+});
 
-//Suppression de plusieurs articles avec une seule requete 
-app.delete("articles/:id", (req, res) =>{
-  i
-})
+//Suppression de plusieurs articles avec une seule requete
+app.delete("/delete/articles", (req, res) => {
+  const selectAll = req.body.select;
+  let articlesAfterDelete = [...data];
+  console.log(articlesAfterDelete);
 
-
+  for (let index = 0; index < selectAll.length; index++) {
+    articlesAfterDelete = articlesAfterDelete.filter(
+      (article) => article.id !== selectAll[index]
+    );
+  }
+  return res.send(articlesAfterDelete);
+});
 
 app.listen(PORT, () => {
   console.log(`Le serveur écoute sur le port ${PORT}`);
